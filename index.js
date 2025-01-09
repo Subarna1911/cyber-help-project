12a7568c3f9b74870e86796861ec221c3a83eaac
subarna1911 <subarnablone09@gmail.com>
2025-01-09 16:38:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// document
+//   .querySelector(".navbar-toggler")
+//   .addEventListener("click", function () {
+//     const navbarItems = document.querySelector(".navbar-item");
+//     navbarItems.classList.toggle("show");
+//   });
+
